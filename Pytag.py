7b5393ddb0cd5b11bd9fa73d5b43a7d42995c6ad
syed abuthahir <abuvanth@gmail.com>
2017-01-14 20:13:39 -0800

    Update and rename pytag.py to Pytag.py

--- Pytag.py
+++ Pytag.py
@@ -1,6 +1,6 @@
 import sys
 f=sys.argv[1] #file name
-tag=sys.argv[2] # name of the tag. eg: html
+tag=sys.argv[2] # name of the tag. eg: option or any html tag
 file1=open(f,"r")
 file2=open("tag.list","a")
 for c in file1.readlines():
